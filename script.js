499f750ff50e86e5fc06c12876d71545201973fe
adinath-r-more <moreadi7777@gmail.com>
2026-01-01 09:41:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const PasswordBox = document.getElementById("Password");
+
+const length = 12;
+
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*([)_+-={}]|\><:;"
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+    let Password = "";
+    Password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    Password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    Password += number[Math.floor(Math.random() * number.length)];
+    Password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (length > Password.length) {
+        Password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    PasswordBox.value = Password;
+}
+
+
+function copyPassword(){
+    PasswordBox.select();
+    document.execCommand("copy")
+}
